7b3aa08b582441090563b0f3f5d133c32d7084f4
RrOoSsSsOo <RrOoSsSsOo@users.noreply.github.com>
2020-04-27 14:32:08 +0200

    Create QCA6174.sh

---
+++ QCA6174.sh
@@ -0,0 +1,5 @@
+sudo mv /lib/firmware/ath10k/QCA6174/hw3.0/board.bin /lib/firmware/ath10k/QCA6174/hw3.0/board.bin.bak
+
+sudo mv /lib/firmware/ath10k/QCA6174/hw3.0/board-2.bin /lib/firmware/ath10k/QCA6174/hw3.0/board-2.bin.bak
+
+sudo cp ./QCA6174/hw3.0/eeprom_ar6320_3p0_TX8_clpc.bin /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
